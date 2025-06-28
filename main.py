8dc818a1770a3f3ce4eed4e5acea12c9efc39390
Ishan <66067552+ishan-jemshid@users.noreply.github.com>
2025-06-28 16:37:45 +0530

    Added a SONG
    The sea Shanty (Wellerman)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def say():
     print("Hello World")
     print("Hi")
-    print("What da HADIN(doggy) doin?")
+    print("There once was a ship that was put to sea the name of the ship was a billy of tea the winds up her bow the boat to blow my bully boy, HUH     Soon may the wellerman come to bring us sugar and tea and rum one day when the tonging is done we'll take our leave'n go")
 say()
